d110c6706d814c86eb82d36a89effbd348f87cbc
Michel.P <michelpr1224@gmail.com>
2023-11-04 18:03:14 -0300

    Requisito 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,26 @@
+import { useSelector } from 'react-redux';
+import { UserType } from '../types';
+
 function Header() {
-  return <div>Header </div>;
+  const { user: { email } } = useSelector((state: UserType) => state);
+  console.log(email);
+
+  return (
+    <div>
+      <span data-testid="email-field">
+        Email:
+        {' '}
+        {email}
+      </span>
+      <span data-testid="total-field">
+        0
+      </span>
+      <span data-testid="header-currency-field">
+        BRL
+      </span>
+
+    </div>
+  );
 }
 
 export default Header;
