17b33cfd0f0f417b734a4ede2dba672774b130a3
Michel.P <michelpr1224@gmail.com>
2023-11-15 13:50:26 -0300

    docs: Remove comments

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -3,8 +3,6 @@ import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-// configure aqui sua store
-
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 if (window.Cypress) {
